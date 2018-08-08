a4104f6deb1a85d7f40f1351d9cf50461ba5a80f
Pooja  Tetali <pooja.tetali@gmail.com>
2018-08-08 16:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const driver = 'bob';
 var ThisCodeWasCalled = "this code was called"; 
 console.log(ThisCodeWasCalled); 
 console.log(driver); 
-console.log(driver, "is the driver ")
+console.log(driver, "is the driver variable name"); 
